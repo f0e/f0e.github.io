b452dd04bacb8f6f5cd93bb4d53e2057ebec01ef
f0e <7321764+f0e@users.noreply.github.com>
2022-05-18 04:54:07 +1000

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+			precompress: true
+		}),
+		prerender: {
+			default: true,
+			onError: ({ status, path, referrer, referenceType }) => {
+				console.warn(`${status} ${path}${referrer ? ` (${referenceType} from ${referrer})` : ''}`);
+			}
+		}
 	},
 	preprocess: sveltePreprocess()
 };
